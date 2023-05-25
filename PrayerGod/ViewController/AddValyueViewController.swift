af68ac303c454ff53aecb8dd96017eccf10c90b4
Manisha Variya <manishavariya111@gmail.com>
2023-05-25 05:37:38 -0700

    show "maximum six digit lable",work complete in add view controller

--- PrayerGod/ViewController/AddValyueViewController.swift
+++ PrayerGod/ViewController/AddValyueViewController.swift
@@ -49,7 +49,7 @@ class AddValyueViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         allmetodeAssignViewDidload()
-       
+        
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -72,6 +72,8 @@ class AddValyueViewController: UIViewController{
         set_lefr_and_right_barButtonItem_Add()
         txt_target_value.delegate = self
         txt_title.delegate = self
+        txt_reminder.delegate = self
+        txt_Start_value.delegate = self
     }
     
     func selected_index_list_on_listVc(){
@@ -83,10 +85,10 @@ class AddValyueViewController: UIViewController{
             txt_Start_value.text = String(data.startValue)
             txt_target_value.text = String(data.targetValue)
             textView_Note.text = data.note
-        
-         }
+            
+        }
     }
-
+    
     func updateName_Value_all_list(){
         var newdatabaseindex_data = databasehelper.sharaintance.getdata()
         newdatabaseindex_data[select_Index_list_Vc_database].title = txt_title.text ?? ""
@@ -94,10 +96,10 @@ class AddValyueViewController: UIViewController{
         newdatabaseindex_data[select_Index_list_Vc_database].startValue = Int16(txt_Start_value.text ?? "") ?? 0
         newdatabaseindex_data[select_Index_list_Vc_database].targetValue = Int16(txt_target_value.text ?? "") ?? 0
         newdatabaseindex_data[select_Index_list_Vc_database].note = textView_Note.text ?? ""
-       
-      }
+        
+    }
     
-func set_lefr_and_right_barButtonItem_Add(){
+    func set_lefr_and_right_barButtonItem_Add(){
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "multiply"), style: .plain, target: self, action: #selector(GoRootVc))
         self.navigationItem.leftBarButtonItem?.tintColor = .white
         
@@ -117,76 +119,64 @@ func set_lefr_and_right_barButtonItem_Add(){
         if self.presentingViewController != nil{
             
             self.dismiss(animated: true, completion: nil)
-         
-        }else{
-            self.navigationController?.popViewController(animated: true)
-            }
-    }
-    
-    
-    @objc func GoFirst_view_controller(){
-       // txt_target_all_textfield(txt_reminder)
-        
-//        if txt_title.text != "" || txt_Start_value.text != "" || txt_target_value.text != "" || txt_reminder.text != "" {
-//
-//                if Start_convert_int >= End_convert_int{
-//                    height_targetvalue.constant = 20
-//                    View_Height_Tragetvalue.constant = 116
-//                    lbl_target_six_digit_value.text = "not enter greter"
-//                }else{
-//                    height_targetvalue.constant = 0
-//                    View_Height_Tragetvalue.constant = 88
-//                }
-////            if End_convert_int <= Start_convert_int{
-////                height_value.constant = 20
-////                View_Height_Value.constant = 116
-////               lbl_strat_Value_six_digit.text = "not enter greter"
-////            }else{
-////                height_value.constant = 0
-////                View_Height_Value.constant = 88
-////               }
-//
-//
-//           }
-       // text_fields_validation(txt_reminder, height_reminder, View_Height_reminder, lbl_reminders_six_digit)
-        if is_selectIndex_bool_Dtabase{
-            if self.presentingViewController != nil{
-                
-                self.dismiss(animated: true, completion: nil)
-                
-            }else{
-                self.navigationController?.popViewController(animated: true)
-                
-            }
-           updateName_Value_all_list()
-            databasehelper.sharaintance.saveItems()
             
         }else{
-            savedata()
-            self.presentingViewController?.dismiss(animated: false, completion: nil)
-            self.presentingViewController?.dismiss(animated: true, completion: nil)
-            self.delegate_addVc?.popupCloseEvent()
-            GlobalData.sharedInstance.isFromSaveTask = true
-            GlobalData.sharedInstance.selectindex = databasehelper.sharaintance.getdata().last
+            self.navigationController?.popViewController(animated: true)
         }
+    }
+    
+    @objc func GoFirst_view_controller(){
+        
+     
+        if txt_title.text == nil{
+            if txt_title.text?.count == 0{
+                lbl_six_digitEnter.text = "This field is required."
+                View_Height_title.constant = 110
+                height_title.constant = 20
+            }else{
+                View_Height_title.constant = 88
+                height_title.constant = 0
+            }
+        }else if txt_Start_value.text != "" && txt_target_value.text != nil{
+            if  Int(txt_Start_value.text ?? "") ?? 0  >= Int( txt_target_value.text ?? "") ?? 0{
+                lbl_target_six_digit_value.text = "Must be greater than the start value."
+                View_Height_Tragetvalue.constant = 110
+                height_targetvalue.constant = 20
+            }else {
+                View_Height_Tragetvalue.constant = 88
+                height_targetvalue.constant = 0
+                
+            }
+        }else if is_selectIndex_bool_Dtabase{
+                if self.presentingViewController != nil{
+                    
+                    self.dismiss(animated: true, completion: nil)
+                    
+                }else{
+                    self.navigationController?.popViewController(animated: true)
+                    
+                }
+                updateName_Value_all_list()
+                databasehelper.sharaintance.saveItems()
+                
+        }else if txt_title.text != "" || txt_reminder.text == "" || txt_Start_value.text == "" || txt_target_value.text != "" {
+                
+                savedata()
+                self.presentingViewController?.dismiss(animated: false, completion: nil)
+                self.presentingViewController?.dismiss(animated: true, completion: nil)
+                self.delegate_addVc?.popupCloseEvent()
+                GlobalData.sharedInstance.isFromSaveTask = true
+                GlobalData.sharedInstance.selectindex = databasehelper.sharaintance.getdata().last
+            }else{
+                print("aqRQW")
+            }
+            
         
     }
-func text_fields_validation( _ textfield : UITextField, _ height : NSLayoutConstraint,_ viewheight : NSLayoutConstraint,_ lbl : UILabel){
-        if textfield.text!.count > 6 {
-            height.constant = 20
-            viewheight.constant = 116
-            lbl.backgroundColor = .white
-        }else{
-            height.constant = 0
-            viewheight.constant = 88
-        }
- 
-    }
-func counting_Range_value_txt(){
     
-  }
+    
     func lbl_height_zero(_ sender : NSLayoutConstraint ,_ txtfield : UITextField){
-    sender.constant = 0
+        sender.constant = 0
     }
     
     func view_height_constrain_zero(_ sender: NSLayoutConstraint,_ txtfield : UITextField){
@@ -200,61 +190,50 @@ func counting_Range_value_txt(){
         
         
     }
-    
-}
-private var kAssociationKeyMaxLength: Int = 0
-extension UITextField {
-
-    @IBInspectable var maxLength: Int {
-        get {
-            if let length = objc_getAssociatedObject(self,&kAssociationKeyMaxLength) as? Int {
-                return length
-            } else {
-                return Int.max
-            }
-        }
-        set {
-            objc_setAssociatedObject(self, &kAssociationKeyMaxLength, newValue, .OBJC_ASSOCIATION_RETAIN)
-            addTarget(self, action: #selector(checkMaxLength), for: .editingChanged)
-        }
-    }
-
-    @objc func checkMaxLength(textField: UITextField) {
-        guard let prospectiveText = self.text,
-            prospectiveText.count > maxLength
-            else {
-                return
-        }
-
-        let selection = selectedTextRange
-
-        let indexEndOfText = prospectiveText.index(prospectiveText.startIndex, offsetBy: maxLength)
-        let substring = prospectiveText[..<indexEndOfText]
-        text = String(substring)
-
-        selectedTextRange = selection
-    }
 }
 extension AddValyueViewController : UITextFieldDelegate{
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
             self.view.endEditing(true)
         }
-//    func textFieldDidBeginEditing(textField: UITextField) -> Bool {
-//        if textField == txt_title {
-//            if textField.text != "" && textField.text!.count > 3{
-//                lbl_six_digitEnter.text = "ijsdfouq"
-//                height_title.constant = 20
-//                View_Height_title.constant = 116
-//            }else{
-//                height_title.constant = 0
-//                View_Height_title.constant = 88
-//            }
-//
-//        }
-//        return true
-//    }
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    
+    let currentCharacterCount = textField.text?.count
 
+    if (range.length + range.location > currentCharacterCount ?? 0 ){
+        return false
+        }
+    let newLength = (currentCharacterCount ?? 0) + string.count - range.length
+       
+        if textField.isEqual(txt_target_value) || textField.isEqual(txt_reminder) || textField.isEqual(txt_Start_value){
+        if txt_reminder.text!.count >= 6{
+            lbl_reminders_six_digit.text = "Maximum 6 digits."
+            height_reminder.constant = 20
+            View_Height_reminder.constant = 110
+        }else if txt_Start_value.text!.count >= 6{
+            lbl_strat_Value_six_digit.text = "Maximum 6 digits."
+            height_value.constant = 20
+            View_Height_Value.constant = 110
+        }else if txt_target_value.text!.count >= 6{
+            lbl_target_six_digit_value.text = "Maximum 6 digits."
+            height_targetvalue.constant = 20
+            View_Height_Tragetvalue.constant = 110
+        }else {
+            height_reminder.constant = 0
+            View_Height_reminder.constant = 88
+            height_value.constant = 0
+            View_Height_Value.constant = 88
+            height_targetvalue.constant = 0
+            View_Height_Tragetvalue.constant = 88
+            height_title.constant = 0
+            View_Height_title.constant = 88
+        }
+    }
+        return true
+    }
+     
+       
+}
        
 
-    }
+    
 
